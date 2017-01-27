52efec514078f89ac5c587a58605c562dd77a59e
Carl Caum <carl@carlcaum.com>
2017-01-26 20:56:35 -0800

    Improve PuppetJobReportNodeV1 events and metrics retrieval

--- src/main/java/org/jenkinsci/plugins/puppetenterprise/apimanagers/PuppetOrchestratorV1/PuppetJobReportV1/PuppetJobReportNodeV1.java
+++ src/main/java/org/jenkinsci/plugins/puppetenterprise/apimanagers/PuppetOrchestratorV1/PuppetJobReportV1/PuppetJobReportNodeV1.java
@@ -7,15 +7,15 @@ import com.google.gson.internal.LinkedTreeMap;
 import org.jenkinsci.plugins.puppetenterprise.apimanagers.puppetorchestratorv1.puppetjobreportv1.PuppetJobReportNodeEventV1;
 import org.jenkinsci.plugins.puppetenterprise.apimanagers.puppetorchestratorv1.puppetjobreportv1.PuppetJobReportNodeMetricsV1;
 
-public class PuppetJobReportNodeV1 {
+public class PuppetJobReportNodeV1 implements Serializable {
   private String node = null;
   private String state = null;
   private Date timestamp = null;
-  private ArrayList<PuppetJobReportNodeEventV1> events = null;
+  private ArrayList<PuppetJobReportNodeEventV1> events = new ArrayList();
   private String certname = null;
   private String transaction_uuid = null;
   private String environment = null;
-  private ArrayList<PuppetJobReportNodeMetricsV1> metrics = null;
+  private PuppetJobReportNodeMetricsV1 metrics = null;
 
   public String getNode() {
     return this.node;
@@ -30,7 +30,13 @@ public class PuppetJobReportNodeV1 {
   }
 
   public ArrayList<PuppetJobReportNodeEventV1> getEvents() {
-    return this.events;
+    //For some reason the orchestrator return null
+    // for the events value instead of an empty array
+    if (this.events == null) {
+      return new ArrayList();
+    } else {
+      return this.events;
+    }
   }
 
   public String getCertname() {
@@ -45,7 +51,7 @@ public class PuppetJobReportNodeV1 {
     return this.environment;
   }
 
-  public ArrayList<PuppetJobReportNodeMetricsV1> getMetrics() {
+  public PuppetJobReportNodeMetricsV1 getMetrics() {
     return this.metrics;
   }
 }
