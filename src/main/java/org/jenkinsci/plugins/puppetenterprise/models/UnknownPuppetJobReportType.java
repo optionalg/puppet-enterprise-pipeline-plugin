f9c300d22039886f68f5f5fa6b46daa33bd670dd
Carl Caum <carl@carlcaum.com>
2017-02-02 22:07:49 -0800

    Impement UnknownPuppetJobReportType exception

---
+++ src/main/java/org/jenkinsci/plugins/puppetenterprise/models/UnknownPuppetJobReportType.java
@@ -0,0 +1,7 @@
+package org.jenkinsci.plugins.puppetenterprise.models;
+
+public class UnknownPuppetJobReportType extends Exception {
+  public UnknownPuppetJobReportType(String message) {
+    super(message);
+  }
+}
